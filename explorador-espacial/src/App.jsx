03f48ead0914f6a57668c63e70a3ef1988a7d25d
Angel <etti.joseangel@gmail.com>
2026-07-17 18:06:32 -0600

    useEffects agregados para simular el consumo de combustible y vuelo de la nave

--- explorador-espacial/src/App.jsx
+++ explorador-espacial/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, use } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 
 function App() {
@@ -8,6 +8,39 @@ function App() {
   const [estadoNave, setEstadoNave] = useState("En órbita");
   const [planetasVisitados, setPlanetasVisitados] = useState([]);
 
+  // Efecto de Montaje y Desmontaje (Simulacion de Vuelo)
+  useEffect(() => {
+    console.log("¡El panel de control está listo!"); // Montaje
+
+    const intervaloVuelo = setInterval(() => {
+      setCombustible((prevCombustible) => {
+        if (prevCombustible > 0) {
+          // Si hay combustible, avanzamos
+          setDistancia((prevDistancia) => prevDistancia + 10);
+          return prevCombustible - 5;
+        } else {
+          // Si nos quedamos sin combustible, detenemos el intervalo
+          setEstadoNave("A la deriva (Sin combustible)");
+          clearInterval(intervaloVuelo);
+          return 0;
+        }
+      });
+    }, 1000);
+
+    // Funcion de limpieza
+    return () => {
+      clearInterval(intervaloVuelo);
+      console.log("El panel de control se ha apagado."); // Desmontaje
+    };
+  }, []); //Array vacio que se ejecuta solo una vez al montar
+
+  // Efecto de Actualizacion (Monitoreo de combustible)
+  useEffect(() => {
+    if (combustible < 100) {
+      console.log(`¡Combustible actualizado! Nivel: ${combustible}%`);
+    }
+  }, [combustible]); // Se ejecuta cada vez que 'combustible' cambia
+
   //Variable memorizada para el estado de la nave
   const mensajeEstado = useMemo(() => {
     return `Estado de la misión: ${estadoNave}`;
@@ -15,12 +48,10 @@ function App() {
 
   // Manejo basico para el boton de aterrizar
   const manejarAterrizaje = () => {
-    setEstadoNave("Aterrizando");
-    // Agregamos planeta generico para probar la UI
-    setPlanetasVisitados([
-      ...planetasVisitados,
-      `Planeta-${planetasVisitados.length - 1}`,
-    ]);
+    if (combustible > 0) {
+      setEstadoNave("Aterrizando");
+      setPlanetasVisitados([...planetasVisitados, `Planeta- ${planetasVisitados.length + 1}`]);
+    }
   };
 
   return (
@@ -49,12 +80,12 @@ function App() {
 
       <button
         onClick={manejarAterrizaje}
+        disabled= {combustible === 0}
         style={{ padding: "0.5rem 1rem", cursor: "pointer" }}
       >
         Aterrizar
       </button>
 
-      {/* Renderizado temporal de la lista de planetas */}
       {planetasVisitados.length > 0 && (
         <div style={{ marginTop: "2rem" }}>
           <h3>Planetas Visitados:</h3>
